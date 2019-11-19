23bee388833a52396897dff777b041312041aea3
thermitex <thermitex@outlook.com>
2019-11-18 22:23:41 -0500

    Implement more of backbone

--- datapath.v
+++ datapath.v
@@ -1,7 +1,12 @@
 module datapath(
-    input [5:0] addr,                       // address loaded from switches
-    input enable,                           // enable datapath to operate
-    input player,                           // 0 when wait black, 1 white
+    input [5:0] e_addr_in,              // address loaded from switches
+    input [6:0] i_addr_in,              // address given internally
+    input [1:0] data_in,                // data to be written
+    input ld_e_addr,                    // load external address from switches
+    input ld_i_addr,                    // load from i_addr_in
+    input player,                       // 0 when wait black, 1 white
+    input ld_data_p,                    // load data according to player
+    input ld_data,                      // load data from data_in
     input reset,
     input clock,
     // ---- for memory ----
@@ -10,13 +15,43 @@ module datapath(
     // black:   2'b01
     // white:   2'b10
     // wall:    2'b11
-    input data_get,                         // data got from memory
-    output reg [6:0] addr_to_mem,           // address to the memory
-    output reg [1:0] data_to_mem,           // data to be sent
-    output reg wren,                        // write enable
+    input write_to_mem,                 // write to memory
+    input data_get,                     // data got from memory
+    output reg [6:0] addr_to_mem,       // address to the memory
+    output reg [1:0] data_to_mem,       // data to be sent
+    output reg wren,                    // write enable
     // --------------------
-    output reg game_end,                    // signaling end of the game
-    output reg ack                          // acknowledge valid move
+    output reg game_end,                // signaling end of the game
+    output reg ack                      // acknowledge valid move
 );
+
+// address of the current move
+reg [6:0] addr;
+// data of the move (using data protocol)
+reg [1:0] data;
+
+// read in addr
+always @(posedge clock) begin
+    if (!reset)
+        addr <= 7'b0;
+    else begin
+        if (ld_e_addr)
+            addr <= 11 + (addr_in >> 3) << 1 + e_addr_in;
+        if (ld_i_addr)
+            addr <= i_addr_in;
+    end
+end
+
+// get data to be written
+always @(posedge clock) begin
+    if (!reset)
+        data <= 2'b0;
+    else begin
+        if (ld_data_p)
+            data <= player ? 2'b10 : 2'b01;
+        if (ld_data)
+            data <= data_in;
+    end
+end
 
 endmodule // datapath
--- main_controller.v
+++ main_controller.v
@@ -8,27 +8,34 @@ module controller(
     output reg player           // 0 when wait black, 1 white
 );
 
-reg [2:0] current_state, next_state; 
+reg [2:0] current_state, next_state;
 
-localparam  S_GAME_INIT         = 2'd0,
-            S_GAME_INIT_WAIT    = 2'b3;
-            S_WAIT_BLACK        = 2'd1,
-            S_WAIT_WHITE        = 2'd2,
+wire init_start, init_done;
+
+localparam  S_GAME_WELC         = 2'd0,
+            S_GAME_WELC_WAIT    = 2'b1;
+            S_GAME_INIT         = 3'b2
+            S_WAIT_BLACK        = 3'd3,
+            S_WAIT_WHITE        = 3'd4,
 
 always @(*)
 begin: state_table 
     case (current_state)
-        S_GAME_INIT:  next_state = go ? S_GAME_INIT_WAIT : S_GAME_INIT;
-        S_GAME_INIT_WAIT: next_state = go ? S_WAIT_BLACK : S_GAME_INIT_WAIT;
+        S_GAME_WELC:  next_state = go ? S_GAME_INIT_WAIT : S_GAME_WELC;
+        S_GAME_WELC_WAIT: next_state = go ? S_GAME_INIT : S_GAME_WELC_WAIT;
+        S_GAME_INIT: next_state = init_done ？ S_WAIT_BLACK : S_GAME_INIT；
         S_WAIT_BLACK: next_state = ack ? S_WAIT_WHITE : S_WAIT_BLACK; 
         S_WAIT_WHITE: next_state = ack ? S_WAIT_BLACK : S_WAIT_WHITE;
-        default:      next_state = S_GAME_INIT;
+        default:      next_state = S_GAME_WELC;
     endcase
 end // state_table
 
 always @(*)
 begin: enable_signals
     case (current_state)
+        S_GAME_INIT: begin
+            init_start = 1;
+        end
         S_WAIT_BLACK: begin
             player = 0;
         end
@@ -42,7 +49,7 @@ end // enable_signals
 always @(posedge clock)
 begin: state_FFs
     if(!reset)
-        current_state <= S_GAME_INIT;
+        current_state <= S_GAME_WELC;
     else
         current_state <= next_state;
 end // state_FFS
